6f091d2f94a11349cb4495cccc0611355e07f627
MuhammadIqbalAhmad <MuhammadIqbalAhmad@users.noreply.github.com>
2019-09-23 16:29:16 +0530

    Bifurcation of one package into "gui" and "dbutil" package.  Completed MySQL connection coding.

---
+++ src/DButil/LoadDriver.java
@@ -0,0 +1,21 @@
+package DButil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class LoadDriver {
+    private static Connection con;
+    public static Connection createConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","root");
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        return con;
+    }
+    public static void main(String[] args) {
+        con = createConnection();
+    }
+}
