253e6ada9fa63ea2a25cb963e19f82982a8c76a7
MuhammadIqbalAhmad <MuhammadIqbalAhmad@users.noreply.github.com>
2019-10-05 17:41:21 +0530

    Updated code in Controller.java

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,24 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Popup;
+
 public class Controller {
+    @FXML protected void newCustomer(ActionEvent Event){
+        Popup popup = new Popup();
+    }
+    @FXML protected void newItem(ActionEvent Event){
+        Popup popup = new Popup();
+    }
+    @FXML protected void newInvoice(ActionEvent Event){
+        Popup popup = new Popup();
+    }
+    @FXML protected void newPayment(ActionEvent Event){
+        Popup popup = new Popup();
+    }
+    @FXML protected void newEstimate(ActionEvent Event){
+        Popup popup = new Popup();
+    }
+
 }
