843b5d190305064c3dd6dadb9226e2a8cfb5f164
MuhammadIqbalAhmad <MuhammadIqbalAhmad@users.noreply.github.com>
2019-10-05 18:05:21 +0530

    Code for basic functionality of New Customer button.

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -2,11 +2,22 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    @FXML protected void newCustomer(ActionEvent Event){
-        Popup popup = new Popup();
+    @FXML protected void newCustomer(ActionEvent Event) throws IOException{
+    Stage stage = new Stage();
+    Parent root = FXMLLoader.load(getClass().getResource("popupNewCustomer.fxml"));
+    Scene scene = new Scene(root, 300, 275);
+    stage.setScene(scene);
+    Popup popup = new Popup();
+    popup.show(stage);
     }
     @FXML protected void newItem(ActionEvent Event){
         Popup popup = new Popup();
